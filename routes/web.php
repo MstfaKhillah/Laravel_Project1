548cfe36e670c11e97fefec86210584a1b5a4407
Mstfa Khilla <mstfakhilla@gmail.com>
2022-11-22 20:38:54 +0200

    mm

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('url', 'Action');
+// Route::post('url', 'Action');
+// Route::put('url', 'Action');
+// Route::patch('url', 'Action');
+// Route::delete('url', 'Action');
+
+Route::get('new', function() {
+    return 'Hello';
 });
+
+Route::get('/', function() {
+    return 'Homepage';
+});
+
+Route::get('user/{name}/{age}/{username}', function($name, $age, $username) {
+    return 'User: ' .$name . ' | Age: ' .$age . ' | User Name: ' . $username;
+})-> whereAlpha('name')->whereNumber('age')->whereAlphaNumeric('username');
